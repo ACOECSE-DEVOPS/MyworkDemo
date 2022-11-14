2ce0edae0751a319c6f1906a1f9c9afbc5a3be99
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-11-14 14:01:49 +0530

    first commit

---
+++ src/test/java/MyWork/TestWork/AppTest.java
@@ -0,0 +1,20 @@
+package MyWork.TestWork;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
